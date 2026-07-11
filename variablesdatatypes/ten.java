4075cc9a81f9d8c9050ee07569a2860387049406
meet patel <pmeet6042@gmail.com>
2026-07-11 15:01:00 +0530

    Add initial implementation of variable types and their outputs in ten.java

---
+++ variablesdatatypes/ten.java
@@ -0,0 +1,25 @@
+package variablesdatatypes;
+
+public class ten {
+    public static void main(String[] args) {
+        byte b = 100;
+        short s = 1000;
+        int i = 10000;
+        double d = 12.0;
+        long l = 10000000L;
+        float f = 10.0f;
+        char ch = 'A';
+        boolean bool = true;
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(d);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(ch);
+        System.out.println(bool);
+
+
+    }
+}
