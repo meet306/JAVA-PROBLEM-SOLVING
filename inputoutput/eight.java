88a1b4d3e0df5867530f207f23932beb560a759c
meet patel <pmeet6042@gmail.com>
2026-07-11 17:29:22 +0530

    Add implementation for reading an integer array size and initializing the array in eight.java

---
+++ inputoutput/eight.java
@@ -0,0 +1,12 @@
+package inputoutput;
+// Read array size.
+import java.util.Scanner;
+public class eight {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int array = sc.nextInt();
+        int[] arr = new int[array];
+
+        System.out.println(array);
+    }
+}
