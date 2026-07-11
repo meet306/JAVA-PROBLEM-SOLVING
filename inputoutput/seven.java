cd77671a0af0bfaafd49c3352666e33946b4b141
meet patel <pmeet6042@gmail.com>
2026-07-11 17:25:15 +0530

    Add implementation for reading a full sentence and displaying it in seven.java

---
+++ inputoutput/seven.java
@@ -0,0 +1,13 @@
+package inputoutput;
+// Read full sentence.
+import java.util.Scanner;
+public class seven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter a line: ");
+        String a = sc.nextLine();
+
+        System.out.println("your enter line is here: "+ a);
+
+    }
+}
