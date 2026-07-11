b9558df88d407c259567e5b37e7e7ffb2eaa628d
meet patel <pmeet6042@gmail.com>
2026-07-11 17:09:08 +0530

    Add implementation for taking float input and printing it in three.java

---
+++ inputoutput/three.java
@@ -0,0 +1,14 @@
+//Take float input.
+
+package inputoutput;
+import java.util.Scanner;
+public class three {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a float number: ");
+        float f = sc.nextFloat();
+
+        System.out.println("your float number is: "+ f);
+
+    }
+}
