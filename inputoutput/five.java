6291e9be8e1b74efc582db6fb1c53aa3f53e697a
meet patel <pmeet6042@gmail.com>
2026-07-11 17:16:49 +0530

    Add implementation for taking multiple integer inputs and displaying them in five.java

---
+++ inputoutput/five.java
@@ -0,0 +1,14 @@
+//Take multiple integers.
+
+package inputoutput;
+import java.util.Scanner;
+public class five {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    System.out.println(a);
+    System.out.println(b);
+    }
+}
