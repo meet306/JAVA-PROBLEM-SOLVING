2884c1979b4b41bed143f1eccc4deb9492da11e4
meet patel <pmeet6042@gmail.com>
2026-07-11 17:39:25 +0530

    Add implementation for reading and displaying decimal numbers and multiple data types in nine.java and ten.java

---
+++ inputoutput/nine.java
@@ -0,0 +1,10 @@
+package inputoutput;
+// Read decimal numbers.
+import java.util.Scanner;
+public class nine {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        System.out.println(a);
+    }
+}
---
+++ inputoutput/ten.java
@@ -0,0 +1,21 @@
+package inputoutput;
+//Input different data types together.
+import java.util.Scanner;
+public class ten {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        
+        char ch = sc.next().charAt(0);
+        
+        double decimal = sc.nextDouble();
+        String name = sc.next();
+
+        System.out.println(num);
+        System.out.println(name);
+        System.out.println(ch);
+        System.out.println(decimal);
+
+    }
+}
