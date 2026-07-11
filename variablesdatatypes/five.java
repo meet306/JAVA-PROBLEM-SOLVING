ab5e315e1efe20d1cffe07c3adb3138ed5e7a04b
meet patel <pmeet6042@gmail.com>
2026-07-11 12:20:58 +0530

    Add implementations for finding ASCII value and converting char to int in five.java and six.java

---
+++ variablesdatatypes/five.java
@@ -0,0 +1,11 @@
+// Find ASCII value of a character.
+
+package variablesdatatypes;
+
+public class five {
+    public static void main(String[]args){
+        char ch = 'A';
+        int ascii = (int)ch;
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+    }
+}
---
+++ variablesdatatypes/six.java
@@ -0,0 +1,11 @@
+// Convert char to int.
+
+package variablesdatatypes;
+
+public class six {
+    public static void main(String[] args) {
+        char ch = '7';
+        int num = ch - '0';
+        System.out.println("The Num value of " + ch + " is: " + num);
+    }
+}
